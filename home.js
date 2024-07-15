4e9c25bfc75339d8a6943aa42fc051a81ee9be8d
Team2enchhash <team2enchhash@gmail.com>
2024-07-15 17:16:58 +0530

    Home Page Docs Uploaded

---
+++ home.js
@@ -0,0 +1,42 @@
+const menu=document.querySelector("#sidebar");
+const overlay=document.querySelector(".aside-overlay");
+
+function openmenu(){
+    menu.style.left="0";
+    overlay.style.display="block";
+}
+function closemenu(){
+    menu.style.left="-100%";
+    overlay.style.display="none"
+}
+
+
+let counters=document.querySelectorAll('.count');
+let Section=document.getElementById("counter-section");
+
+
+let CounterObserver= new IntersectionObserver(
+    (entries,observer)=>{
+        let[entry]=entries;
+        if(!entry.isIntersecting)return;
+            let speed=97;
+            counters.forEach((counter)=>{
+                function updateCounter(){
+                    const targetNumber=+counter.dataset.target;
+                    const initialNumber=+counter.innerText;
+                    const incPerCount=targetNumber/speed;
+                    if(initialNumber<targetNumber){
+                        counter.innerText=Math.ceil(initialNumber+incPerCount);
+                        setTimeout(updateCounter,40);
+                    }
+                }
+                updateCounter();
+            });
+        
+    },{
+        root:null,
+        threshold:0.4,
+    }
+);
+
+CounterObserver.observe(Section);
