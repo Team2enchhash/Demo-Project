8d9407f7874bbfd6eeb41d79fba371f899f2ba7b
Team2enchhash <team2enchhash@gmail.com>
2024-07-15 23:04:53 +0530

    Animation added to home page

--- home.js
+++ home.js
@@ -40,3 +40,21 @@ let CounterObserver= new IntersectionObserver(
 );
 
 CounterObserver.observe(Section);
+
+
+window.addEventListener('scroll',reveal);
+
+function reveal(){
+    var reveals=document.querySelectorAll('.reveal');
+
+    for(var i=0;i<reveals.length;i++){
+        var windowheight=window.innerHeight;
+        var revealtop=reveals[i].getBoundingClientRect().top;
+        var revealpoint=-20;
+        if(revealtop<windowheight-revealpoint){
+            reveals[i].classList.add('active');
+        }else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
